1a8e6c677db638e2a70c520e5ae011d1347c1fcc
zoro <aoping@yougola.com>
2018-12-13 20:59:44 +0800

    管理资源-回退处理

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import _ from 'lodash'
-import './style.css'
-import Icon from './icon.png'
-import Data from './data.xml'
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello')
 
-  // 
-  // var Img = new Image()
-  // Img.src = Icon
-  // element.appendChild(Img)
-
-  console.log(Data)
   return element;
 }
 
