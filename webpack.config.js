5edf80bbca04c7b80c93e664cc43842ea5f1bb46
zoro <aoping@yougola.com>
2018-12-14 12:00:48 +0800

    开发-使用 webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,9 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
