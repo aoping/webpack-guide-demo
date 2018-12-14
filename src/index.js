71e826ba72c008dd103453e08f7afa91bfb4901f
zoro <aoping@yougola.com>
2018-12-14 15:24:52 +0800

    模块热更新-启用HMR

--- src/index.js
+++ src/index.js
@@ -9,9 +9,19 @@ function component() {
 
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
-  
+
   element.appendChild(btn);
   return element;
 }
 
-document.body.appendChild(component());
+let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
+document.body.appendChild(element);
+
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('x')
+    document.body.removeChild(element);
+    element = component(); // 重新渲染页面后，component 更新 click 事件处理
+    document.body.appendChild(element);
+  })
+}
