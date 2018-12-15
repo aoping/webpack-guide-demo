0cae07c53c1d16e21110a8e7f2ec4cc10e043edf
zoro <aoping@yougola.com>
2018-12-15 13:22:02 +0800

    创建library-基本配置

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-function component() {
-  var element = document.createElement('div');
+import _ from 'lodash';
+import numRef from './ref.json';
 
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+export function numToWord(num) {
+  return _.reduce(numRef, (accum, ref) => {
+    return ref.num === num ? ref.word : accum;
+  }, '');
 }
 
-document.body.appendChild(component());
+export function wordToNum(word) {
+  return _.reduce(numRef, (accum, ref) => {
+    return ref.word === word && word.toLowerCase() ? ref.num : accum;
+  }, -1);
+}
