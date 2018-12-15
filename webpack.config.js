2fe3c41ee769d49afdbf7bbc3df597eb7aa0612c
zoro <aoping@yougola.com>
2018-12-16 00:17:02 +0800

    使用环境变量

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path')
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-}
+module.exports = env => {
+  // Use env.<YOUR VARIABLE> here:
+  console.log('NODE_ENV: ', env.NODE_ENV); // 'local'
+  console.log('Production: ', env.production); // true
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    }
+  };
+};
