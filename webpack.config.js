54c2f77626e18ed80b41e2833474592ca20c60a1
zoro <aoping@yougola.com>
2018-12-15 15:47:51 +0800

    shimming-shmming全局变量lodash

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path')
+const webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      _: 'lodash'
+    })
+  ]
 }
