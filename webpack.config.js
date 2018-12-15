0f91dd35eef497f7468fbbfcf2a95b10da1fd721
zoro <aoping@yougola.com>
2018-12-16 00:04:28 +0800

    typescript-source map

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,21 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
 }
